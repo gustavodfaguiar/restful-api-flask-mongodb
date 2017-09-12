64cc8144d078281c9a2a354f620e24abbf3ca983
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-09-12 14:54:17 -0300

    Add method add one verb POST

--- app.py
+++ app.py
@@ -11,11 +11,17 @@ def test():
 def return_all():
     return jsonify({'languages': languages})
 
-
 @app.route('/lang/<string:name>', methods=['GET'])
 def return_one(name):
     langs = [language for language in languages if language['name'] == name]
-    return jsonify({'language': langs})
+    return jsonify({'language': langs[0]})
+
+@app.route('/lang', methods=['POST'])
+def add_one():
+    language = {'name': request.json['name']}
+    languages.append(language)
+    return jsonify({'languages': languages})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)

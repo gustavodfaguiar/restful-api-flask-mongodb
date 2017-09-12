5876e2c2abb0d48258bed16b912fbb1163248ef6
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-09-12 14:43:37 -0300

    Add route lang -> list all languages and lang/name -> get a language

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+languages = [{'name': 'Javascript',}, {'name': 'Python'}, {'name': 'Ruby'}]
+
 @app.route('/', methods=['GET'])
 def test():
     return jsonify({'message': 'It works! irrru!'})
+
+@app.route('/lang', methods=['GET'])
+def return_all():
+    return jsonify({'languages': languages})
+
+
+@app.route('/lang/<string:name>', methods=['GET'])
+def return_one(name):
+    langs = [language for language in languages if language['name'] == name]
+    return jsonify({'language': langs})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)

353915b4dd3ff1a26aacf9d80fd5b9472ff96552
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-09-13 14:08:03 -0300

    Create method edit with verb PUT

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ def add_one():
     languages.append(language)
     return jsonify({'languages': languages})
 
+@app.route('/lang/<string:name>', methods=['PUT'])
+def edit_one(name):
+    langs = [language for language in languages if language['name'] == name]
+    langs[0]['name'] = request.json['name']
+    return jsonify({'languages': langs[0]})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
